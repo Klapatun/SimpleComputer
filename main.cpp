a08757875493ab1379198f006bb1af8a603d7d0d
Klapatun <klapatuna@mail.ru>
2019-09-24 19:25:50 +0700

    feat: added clear and home sequence

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 **
 **
 */
+#include <string.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <signal.h>
 
+#define ESC	"\033"
 
 int main(){
+
+	printf(ESC "[2J"); 	//clear
+	printf(ESC "[H");	//home
 
     return 0;
 }
