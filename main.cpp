8c1850a3e43a6f5e654a0617d27fea9c0eb2539d
Klapatun <klapatuna@mail.ru>
2019-10-02 15:46:37 +0700

    feat: Added define MyBigChars and complated MyTerm

--- main.cpp
+++ main.cpp
@@ -7,80 +7,171 @@
 #include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <termios.h>
 
 using namespace std;
 
 /*#define ESC	"\033"*/
 
 
-class TermLinux {
-	private:
-		const char* CLEAR = "\E[2J";
-		const char* GOTOXY = "\E%d;%dH";
-		const char* SETFCOLOR = "\E[3%dm";
-		const char* SETBCOLOR = "\E[4%dm";
-		const char* ESCSPACE = "\E[1C";
-		const char* ESC = "\E";
-	public:
+namespace TermLinux {
 
-		void home(void) {
-			cout << ESC << "[H";
-		}
 
-		int mt_clrscr(void) {
+    enum {
+        RED 	= 1,
+        GREEN,
+        YELLOW,
+        BLUE,
+        MAGENTA,
+        DEFAULT	= 9
+    };
 
-			int term = open("/dev/tty", O_RDWR);
+    class MyTerm {
+        private:
+            const char* CLEAR = "\E[2J";
+            const char* GOTOXY = "\E%d;%dH";
+            const char* SETFGCOLOR = "\E[3%dm";
+            const char* SETBGCOLOR = "\E[4%dm";
+            const char* ESCSPACE = "\E[1C";
+            const char* ESC = "\E";
+        public:
 
-			if (term == -1) {
-				cerr << "Terminal Error!";
-				close(term);
-				return -1;
-			}
-			
-			write(term, CLEAR, sizeof(CLEAR));
-			close(term);
-			return 0;
-		}
+            int mt_clrscr(void) {
 
-		int mt_gotoXY(int x, int y) {
-			
-			int term = open("/dev/tty", O_RDWR);
+                int term = open("/dev/tty", O_RDWR);
 
-			if (term == -1) {
-				cerr << "Terminal Error!";
-				close(term);
-				return -1;
-			}
-			char buff[50];
-			
-			sprintf(buff, GOTOXY, x, y);
+                if (term == -1) {
+                    cerr << "mt_clrscr: Terminal Error!";
+                    close(term);
+                    return -1;
+                }
 
-			write(term, buff, strlen(buff));
-			close(term);
-			return 0;
-		}
+                write(term, CLEAR, sizeof(CLEAR));
+                close(term);
+                return 0;
+            }
 
-		int mt_getscreensize (int* rows, int* cols) {
-			return 0;
-		}
+            int mt_gotoXY(int x, int y) {
 
-		//int mt_setfgcolor (enum colors) {
-		//	return 0;
-		//}
+                int term = open("/dev/tty", O_RDWR);
 
-		//int mt_setbgcolor (enum colors) {
-		//	return 0;
-		//}
-		
-		TermLinux(void) {
+                if (term == -1) {
+                    cerr << "mt_gotoXY: Terminal Error!";
+                    close(term);
+                    return -1;
+                }
+                char buff[50];
 
-		}
-};
+                sprintf(buff, GOTOXY, x, y);
+
+                write(term, buff, strlen(buff));
+                close(term);
+                return 0;
+            }
+
+            int mt_getscreensize (int* rows, int* cols) {
+
+                struct winsize ws;
+
+                if (!ioctl (1, TIOCGWINSZ, &ws)) {
+                    *rows = ws.ws_row;
+                    *cols = ws.ws_col;
+                }else {
+                    cerr << "mt_getscreensize: Error!";
+                    return -1;
+                }
+
+                return 0;
+            }
+
+            int mt_setfgcolor (uint8_t color) {
+
+                int term = open("/dev/tty", O_RDWR);
+
+                if (term == -1) {
+                    cerr << "mt_setfgcolor: Terminal Error!";
+                    close(term);
+                    return -1;
+                }
+
+                char buff[50];
+
+                sprintf(buff, SETFGCOLOR, color);
+                write(term, buff, strlen(buff));
+
+                close(term);
+
+                return 0;
+            }
+
+            int mt_setbgcolor (uint8_t color) {
+
+                int term = open("/dev/tty", O_RDWR);
+
+                if (term == -1) {
+                    cerr << "mt_setbgcolor: Terminal Error!";
+                    close(term);
+                    return -1;
+                }
+
+                char buff[50];
+
+                sprintf(buff, SETBGCOLOR, color);
+                write(term, buff, strlen(buff));
+
+                close(term);
+                return 0;
+            }
+
+    };
+
+    class MyBigChars {
+
+    private:
+
+
+    public:
+
+        int bc_printa(const char* str) {
+            return 0;
+        }
+
+        int bc_box(int x1, int y1, int x2, int y2) {
+            return 0;
+        }
+        /*
+        int bc_printbigchar(const int* src, int x, int y, enum Colors font, enum Colors bgoaund) {
+            return 0;
+        }
+        */
+        int bc_setbigcharpos(int *big, int value) {
+            return 0;
+        }
+
+        int bc_getbigcharpos(const int *big, int* value) {
+            return 0;
+        }
+
+        int bc_bigcharwrite(const int* big) {
+            return 0;
+        }
+
+        int bc_bigcharread(int* big) {
+            return 0;
+        }
+    };
+
+}
+
+
+
 
 int main(){
 
-	TermLinux l;
-
+    TermLinux::MyTerm l;
+    l.mt_setfgcolor(TermLinux::RED);
 	l.mt_clrscr();
 
 	cout << "Hello\n";
